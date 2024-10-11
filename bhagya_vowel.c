f788c0875b611b08eecf1ab6c9663b1cae13ac26
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2024-10-11 23:22:16 +0530

    update-bhagya_vowel.c
    Just edited the file on how to add vowels in a string
    
    Signed-off-by: Bhagya Vardhan <bhagyavardhan13@gmail.com>

--- bhagya_vowel.c
+++ bhagya_vowel.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
-int main()
-{
-    printf("Hello");
+#include <ctype.h>  
+int countVowels(char str[]) {
+    int count = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        char ch = tolower(str[i]); 
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+        i++;
+    }
+    
+    return count;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    int vowels = countVowels(str);
+    printf("Number of vowels: %d\n", vowels);
+    
     return 0;
 }
