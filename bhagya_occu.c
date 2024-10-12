506470c7ec1b5303405fa512698a8e9007a8686c
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2024-10-12 09:37:52 +0530

    Update bhagya_occu.c
    Just updated bhagya_occu.c with better code in it 
    
    Signed-off-by: Bhagya Vardhan <bhagyavardhan13@gmail.com>

--- bhagya_occu.c
+++ bhagya_occu.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-int main() 
-{
-    // finding occurence of character in a string 
-    int i,j;
+int count_occurrences(const char *str, char ch) {
+    int count = 0;
+    while (*str) {
+        if (*str == ch) {
+            count++;
+        }
+        str++;
+    }
+    
+    return count;
+}
+
+int main() {
+    char input_string[100];
+    char character;
+    printf("Enter a string: ");
+    fgets(input_string, sizeof(input_string), stdin);
+    printf("Enter the character to find: ");
+    scanf("%c", &character);
+    int occurrence_count = count_occurrences(input_string, character);
+    printf("The character '%c' occurred %d time(s) in the string.\n", character, occurrence_count);
+    
     return 0;
 }
