625e48c78eab7f07435f5824e7aef75c4c7c4e36
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2024-10-12 09:36:13 +0530

    Update bhagya_char.c
    Just updated the code in bhagya_char.c
    
    Signed-off-by: Bhagya Vardhan <bhagyavardhan13@gmail.com>

--- bhagya_char.c
+++ bhagya_char.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+char first_non_repeating_char(const char *str) {
+    int count[256] = {0}; 
+    for (int i = 0; str[i] != '\0'; i++) {
+        count[(unsigned char)str[i]]++;
+    }
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (count[(unsigned char)str[i]] == 1) {
+            return str[i];
+        }
+    }
+
+    return '\0'; 
+}
 
 int main() {
+    char input_string[100];
+    printf("Enter a string: ");
+    fgets(input_string, sizeof(input_string), stdin);
+    char result = first_non_repeating_char(input_string);
     
+    if (result) {
+        printf("The first non-repeating character is: %c\n", result);
+    } else {
+        printf("No non-repeating character found.\n");
+    }
+
+    return 0;
 }
