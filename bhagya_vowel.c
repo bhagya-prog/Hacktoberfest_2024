170b7c913a39626fdd8c67dab1836c6a2560e965
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2024-10-12 09:34:12 +0530

    Update bhagya_vowel.c
    Just updated the code in bhagya_vowel.c 
    
    Signed-off-by: Bhagya Vardhan <bhagyavardhan13@gmail.com>

--- bhagya_vowel.c
+++ bhagya_vowel.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
-int main()
-{
-    printf("Hello");
+#include <ctype.h>
+int count_vowels(const char *str) {
+    int count = 0;
+    char ch;
+    while (*str) {
+        ch = tolower(*str);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+        
+        str++;
+    }
+    
+    return count;
+}
+
+int main() {
+    char input_string[100];
+    printf("Enter a string: ");
+    fgets(input_string, sizeof(input_string), stdin);
+    int vowel_count = count_vowels(input_string);
+    printf("Number of vowels: %d\n", vowel_count);
+    
     return 0;
 }
